08ee54ef39ea3acfea1c51d82b8e81587ee25fc1
yanan990329 <1431796149@qq.com>
2020-08-07 18:14:07 +0800

    添加

---
+++ pages/components/toggle/toggle.js
@@ -0,0 +1,48 @@
+// pages/components/toggle.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    content: {
+      type: String,
+      // 初始值
+      value: '是否切换为招聘官身份'
+    },
+    confirmText: {
+      type: String,
+      value: '跳转'
+    },
+    cancelText: {
+      type: String,
+      value: '注册'
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isShow: false,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    show() {
+      var show = this.data.isShow
+      this.setData({
+        isShow: !show
+      })
+    },
+    _cancel() {
+      this.triggerEvent("cancel");
+    },
+    _confirm() {
+      //触发成功回调
+      this.triggerEvent("confirm");
+    }
+  },
+
+})
